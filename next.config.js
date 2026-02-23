211b473e5422c1c7c00c6697ac5f5b196627c345
Zsuzsi <mate.zsuzsi89@gmail.com>
2026-02-23 20:46:45 +0100

    Fix: Chromium bin fájlok belefoglalása Vercel deploy-ba

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+    outputFileTracingIncludes: {
+      "/api/export-pdf": ["./node_modules/@sparticuz/chromium/**"],
+    },
+  },
+};
 
 module.exports = nextConfig;
